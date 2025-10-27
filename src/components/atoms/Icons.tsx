cfc7b0115122d933f1d5c48e5d458a6008ed3e03
Muntean Razvan <munteanr20@gmail.com>
2025-10-27 15:19:06 +0200

    Random

--- src/components/atoms/Icons.tsx
+++ src/components/atoms/Icons.tsx
@@ -5,6 +5,7 @@ export const CheckIcon = () => (
   </svg>
 );
 
+
 // Iconiță PLACEHOLDER pentru cardurile de echipă
 export const UserIcon = () => (
   <svg className="w-24 h-24 mx-auto text-gray-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
