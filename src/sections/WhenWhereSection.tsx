34d3f0d8f909309790a74851fe3a3073b348e251
Muntean Razvan <munteanr20@gmail.com>
2025-10-27 15:23:07 +0200

    Random1

--- src/sections/WhenWhereSection.tsx
+++ src/sections/WhenWhereSection.tsx
@@ -7,11 +7,12 @@ export const WhenWhereSection = () => (
     <div className="flex flex-col md:flex-row justify-center gap-8 text-center">
       <div className="rounded-lg bg-mordor-black p-6 flex-1">
         <h3 className="text-2xl font-semibold text-rohan-gold mb-2">CÂND</h3>
-        <p className="text-xl text-gondor-silver">31 Decembrie 2024 - 02 Ianuarie 2025</p>
+        <p className="text-xl text-gondor-silver">31 Decembrie 2025 - 02 Ianuarie 2026</p>
       </div>
       <div className="rounded-lg bg-mordor-black p-6 flex-1">
         <h3 className="text-2xl font-semibold text-rohan-gold  mb-2">UNDE</h3>
         <p className="text-xl text-gondor-silver">La Cabana Șmecheră 🌲</p>
+        <a href="https://maps.app.goo.gl/q1xs6L8KebZ9CAcu5" className="text-xl text-gondor-silver"> &#62;  Asociația Ceangăilor Bârsa &#60;</a>
       </div>
     </div>
   </section>
